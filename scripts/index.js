6cd78896174794a7df350e2df0859b9825e759f1
koldovsky <koldovsky@yahoo.com>
2015-12-15 18:07:11 +0000

    Added pressure, icons

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+$(function() {
+    $.getJSON('http://api.openweathermap.org/data/2.5/forecast/daily?q=Melbourne' + 
+              '&units=metric&cnt=3&appid=2de143494c0b295cca9337e1e96b00e0', 
+        function(data) {
+            $('#tempToday').html(data.list[0].temp.day);
+            $('#tempTomorrow').html(data.list[1].temp.day);
+            $('#tempAfterTomorrow').html(data.list[2].temp.day);
+            $('#pressureToday').html(data.list[0].pressure);
+            $('#pressureTomorrow').html(data.list[1].pressure);
+            $('#pressureAfterTomorrow').html(data.list[2].pressure);
+            $('#iconToday').html('<img src="images/'+ data.list[0].weather[0].icon + '.png" alt="Weather icon">');
+            $('#iconTomorrow').html('<img src="images/'+ data.list[1].weather[0].icon + '.png" alt="Weather icon">');
+            $('#iconAfterTomorrow').html('<img src="images/'+ data.list[2].weather[0].icon + '.png" alt="Weather icon">');
+        }
+    );
+});
